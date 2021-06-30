e5d96df48ab37eb3ac2c141fba925777ed4c917f
Rrch <rrch.cr8@gmail.com>
2021-06-30 19:07:25 -0400

    initial commit

---
+++ tresuno/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tresuno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
